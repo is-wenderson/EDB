1d41c5c3ea18d6b5715411454d4fdfd75686c5a7
Wenderson <wendersoncunha176@gmail.com>
2024-09-23 19:51:49 -0300

    soma diagonal matriz

---
+++ questao4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int somarDiagonais(int matriz[][3], int n) {
+    int soma = 0;
+    for (int i = 0; i < n; i++) {
+        soma += matriz[i][i];
+
+    }
+    return soma;
+}
+int main(){
+    int matriz[3][3], n = 3;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            scanf("%d", &matriz[i][j]);    
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    int soma = somarDiagonais(matriz, n);
+    printf("Soma das diagonais: %d\n", soma);
+}
